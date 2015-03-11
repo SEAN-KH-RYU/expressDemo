cf6e6c2f6c8a036b16ab0611afd7cb150f14761b
Kyounghyun Ryu <eva700ca@gmail.com>
2015-03-11 15:29:08 +0000

    added contact me partial and a new route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'expressDemo' });
+  res.render('index', { title: 'expressDemo', firstHeadline: "Here's my first headline" });
+});
+
+/* GET about page */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: "About Me", firstHeadline: "Example About Me BIO" });
+});
+
+/* GET contact page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: "Contact Me", firstHeadline: "Please enter your info" });
 });
 
 module.exports = router;
